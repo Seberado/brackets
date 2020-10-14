239e61a58492d44592575dd7b2af72065e2886c5
Andrey Sinaf <andre.as2016e@gmail.com>
2020-10-14 22:43:45 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketsDict = new Map(bracketsConfig);
+
+    let stackArr = [];
+
+    for (let bracket of str) {
+        if (bracketsDict.has(bracket) &&
+            bracketsDict.get(bracket) !== bracket) {
+            stackArr.push(bracket);
+        } else if (bracketsDict.get(bracket) &&
+                    bracketsDict.get(bracket) === bracket &&
+                    stackArr[stackArr.length - 1] !== bracket) {
+            stackArr.push(bracket);
+        } else if (bracketsDict.get(bracket) &&
+                    bracketsDict.get(bracket) === bracket &&
+                    stackArr[stackArr.length - 1] === bracket) {
+            stackArr.pop();
+        } else {
+            if (stackArr.length < 1) {
+                return false;
+            } else if (bracketsDict.get(stackArr[stackArr.length - 1]) !== bracket) {
+                return false;
+            } else {
+                stackArr.pop();
+            }
+        }
+    }
+    return (stackArr.length === 0) ? true : false;
 }
